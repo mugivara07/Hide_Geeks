25e4143acdb4d6fbfc085d01c36aaa3df3d61476
Artem-Mo <super.art351@yandex.ru>
2022-04-20 05:22:12 -0700

    moving example

--- main.cpp
+++ main.cpp
@@ -2,23 +2,36 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
+
+    sf::Texture texture;
+    texture.loadFromFile("include/img.png");
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+    sprite.setPosition(200, 200);
 
     while (window.isOpen())
     {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+            sprite.move(-0.1,0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+            sprite.move(0.1,0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+            sprite.move(0,-0.1);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+            sprite.move(0,0.1);
+        }
         window.clear();
-        window.draw(shape);
+        window.draw(sprite);
         window.display();
     }
-
     return 0;
 }
